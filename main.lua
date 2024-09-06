692853374d6406e012ed1601f746e9f50314ec55
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-09-06 19:48:31 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
-local GameName = "Nexer Hub - Where Guide Resides 👏"
+local GameName = "Where Guide Resides 👏"
 
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({IntroText = "Nexer Hub 👏", IntroIcon = "rbxassetid://15315284749",Name = GameName, HidePremium = false, SaveConfig = true, ConfigFolder = "Tutorial"})
+local Window = OrionLib:MakeWindow({IntroText = "Where Guide Resides 👏", IntroIcon = "rbxassetid://15315284749",Name = GameName, HidePremium = false, SaveConfig = true, ConfigFolder = "Tutorial"})
 
 OrionLib:MakeNotification({Name = "Warning",Content = "Use at your own risk.",Image = "rbxassetid://7733658504",Time = 5})
 
